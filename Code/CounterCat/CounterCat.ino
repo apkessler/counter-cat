bb9be966ac73915e564d73266e3088db63d90a10
Andrew Kessler <andrewkessler6@gmail.com>
2014-02-06 18:25:44 -0800

    Implemented scanning eye, added more LED colors. Updated to Rev3 board

--- Code/CounterCat/CounterCat.ino
+++ Code/CounterCat/CounterCat.ino
@@ -23,18 +23,29 @@
 /////////////////////////////////////////////
 
 
-#define SENSOR_1_PIN   13
-#define SENSOR_2_PIN   7
-#define BLENDER_PIN    12
-#define GREEN_LED_PIN  5
-#define RED_LED_PIN    4
-#define BUTTON_PIN     2
+//These are on the "analog" arduino ports
+#define BUTTON_PIN   A0 //Port C0
+#define BLENDER_PIN  A1 //Port C1
+#define SENSOR_1_PIN A2 //Port C2
+#define SENSOR_2_PIN A3 //Port C3
 
-//The DIP switches!
-#define SWITCH_1_PIN  11
-#define SWITCH_2_PIN  10
-#define SWITCH_3_PIN  9
-#define SWITCH_4_PIN  8
+//The LEDs
+#define RED_LED_PIN  A4 //Port C4
+#define GRN_LED_PIN  A5 //Port C5
+#define BLU_LED_PIN 2 //Port D2
+#define YLW_LED_PIN 3 //Port D3
+
+
+#define COOLDOWN_LED_PIN BLU_LED_PIN
+#define WARMUP_LED_PIN YLW_LED_PIN
+#define ARMED_LED_PIN RED_LED_PIN
+#define PAUSED_LED_PIN GRN_LED_PIN
+
+//The Decoder - 74HC238
+#define DECODER_LINE0 4 //Port D4
+#define DECODER_LINE1 5 //Port D5
+#define DECODER_LINE2 6 //Port D6
+#define DECODER_ENABLE 7 //Port D7
 
 
 //Note that the PIR sensor needs at least 40 seconds to start up. 
@@ -46,13 +57,26 @@
 #define COOLDOWN_FLICKER  200
 #define BUTTON_DEBOUNCE 500
 
-#define USE_PIR2 false
+#define EYE_SCANNING_RATE 10 
+
+#define USE_PIR2 true
+
+#define BIT0HI 0x01
+#define BIT1HI 0x02
+#define BIT2HI 0x04
+#define BIT3HI 0x08
+#define BIT4HI 0x0F
+#define BIT5HI 0x20
+#define BIT6HI 0x40
+#define BIT7HI 0x80
+
+
 
 //These are the states for our state machine. 
 enum State
 {
   S_WARMING_UP,
-  S_IDLE,
+  S_ARMED,
   S_ACTIVE,
   S_COOLDOWN,
   S_PAUSED,
@@ -81,28 +105,42 @@ int sensorThen;
 int safetyThen;
 boolean wasHigh = false;
 
+
+
 ///SETUP//////////////////////////////////////////////////////////////////
 void setup(void)
 {
-
+  //Since this controls the relay, best to set it low first
   pinMode(BLENDER_PIN, OUTPUT);
+  digitalWrite(BLENDER_PIN, LOW);
+  
+  //All the inputs
   pinMode(SENSOR_1_PIN, INPUT);  
   pinMode(SENSOR_2_PIN, INPUT);  
   pinMode(BUTTON_PIN, INPUT);
-  pinMode(GREEN_LED_PIN, OUTPUT);
+
+  //Set all the LEDs as outputs, and let low to start
   pinMode(RED_LED_PIN, OUTPUT);
+  digitalWrite(RED_LED_PIN, LOW);  
   
-  pinMode(SWITCH_1_PIN, INPUT);
-  pinMode(SWITCH_2_PIN, INPUT);
-  pinMode(SWITCH_3_PIN, INPUT);
-  pinMode(SWITCH_4_PIN, INPUT);
-
-  digitalWrite(BLENDER_PIN, LOW);
-  digitalWrite(GREEN_LED_PIN, LOW);
-  digitalWrite(RED_LED_PIN, LOW);
-
+  pinMode(GRN_LED_PIN, OUTPUT);
+  digitalWrite(GRN_LED_PIN, LOW);  
+  
+  pinMode(YLW_LED_PIN, OUTPUT);
+  digitalWrite(YLW_LED_PIN, LOW);  
+  
+  pinMode(BLU_LED_PIN, OUTPUT);
+  digitalWrite(BLU_LED_PIN, LOW);  
+  
+  pinMode(DECODER_LINE0,OUTPUT);
+  pinMode(DECODER_LINE1,OUTPUT);
+  pinMode(DECODER_LINE2,OUTPUT);
+  
+  pinMode(DECODER_ENABLE, OUTPUT);
+  digitalWrite(DECODER_ENABLE, LOW);
+  
   Serial.begin(9600);
-  Serial.println("Starting CounterCat2...");
+  Serial.println("Starting CounterCat3...");
   if (USE_PIR2)
   {
     Serial.println("Using PIR2 as safety.");
@@ -127,6 +165,12 @@ void loop(void)
 {
   now = millis();
 
+  //Step the scanning eye.
+  if (now % EYE_SCANNING_RATE == 0)
+  {
+      stepScanningEye();
+   } 
+  
   /*
    * Check the state of the button.
    */
@@ -185,12 +229,12 @@ void loop(void)
     {
       if (wasHigh)
       {
-        digitalWrite(RED_LED_PIN, LOW);
+        digitalWrite(WARMUP_LED_PIN, LOW);
         wasHigh = false;
       } 
       else
       {
-        digitalWrite(RED_LED_PIN, HIGH); 
+        digitalWrite(WARMUP_LED_PIN, HIGH); 
         wasHigh = true;
       }
     }
@@ -198,16 +242,17 @@ void loop(void)
     if (now > (timerStart + WARM_UP_TIME_MS))
     {
       Serial.println("Warmed up!");
-      currentState = S_IDLE;
+      currentState = S_ARMED;
+      digitalWrite(WARMUP_LED_PIN,LOW); //make sure this pin goes off
     }
     break;
 
 
-  case S_IDLE:
+  case S_ARMED:
 
     //Leave the red light on while we're armed
-    digitalWrite(RED_LED_PIN, HIGH);
-    digitalWrite(GREEN_LED_PIN, LOW);
+    turnOffAllLEDs();
+    digitalWrite(ARMED_LED_PIN, HIGH);
 
     //If there's a rising edge on the sensor pin
     if (sensorRising)
@@ -231,14 +276,15 @@ void loop(void)
     {
       if (wasHigh)
       {
-        digitalWrite(RED_LED_PIN, LOW);
-        digitalWrite(GREEN_LED_PIN, LOW);
+        turnOffAllLEDs();
         wasHigh = false;
       } 
       else
       {
         digitalWrite(RED_LED_PIN, HIGH); 
-        digitalWrite(GREEN_LED_PIN, HIGH);
+        digitalWrite(GRN_LED_PIN, HIGH);
+        digitalWrite(YLW_LED_PIN, HIGH); 
+        digitalWrite(BLU_LED_PIN, HIGH);
         wasHigh = true; 
       }
     }
@@ -250,23 +296,25 @@ void loop(void)
       digitalWrite(BLENDER_PIN, LOW);
       timerStart = now;
       currentState = S_COOLDOWN; 
+      turnOffAllLEDs();
+      
     }
     break;
 
   case S_COOLDOWN:
 
-    digitalWrite(RED_LED_PIN, LOW);
+    turnOffAllLEDs();
     
     if (now % COOLDOWN_FLICKER == 0)
     {
       if (wasHigh)
       {
-        digitalWrite(GREEN_LED_PIN, LOW);
+        digitalWrite(COOLDOWN_LED_PIN, LOW);
         wasHigh = false;
       }
       else
       {
-        digitalWrite(GREEN_LED_PIN, HIGH);
+        digitalWrite(COOLDOWN_LED_PIN, HIGH);
         wasHigh = true; 
       }
 
@@ -275,22 +323,22 @@ void loop(void)
     if (now > (timerStart + COOLDOWN_TIME_MS))
     {
       Serial.println("Done cooling down.");
-      currentState = S_IDLE;  
+      currentState = S_ARMED;  
     }
     break;
 
   case S_PAUSED:
 
-    digitalWrite(GREEN_LED_PIN, HIGH);
-    digitalWrite(RED_LED_PIN, LOW);
-    digitalWrite(BLENDER_PIN, LOW);
-    
+   turnOffAllLEDs();
+   digitalWrite(PAUSED_LED_PIN, HIGH);
+   
     if (now > (timerStart + BUTTON_DEBOUNCE))
     {
       if (buttonPushed)
       {
         Serial.println("Unpaused!");
-        currentState = S_IDLE;          
+        currentState = S_ARMED;          
+        turnOffAllLEDs();
       }
     }
     break;
@@ -300,3 +348,38 @@ void loop(void)
 
 }
 
+
+
+
+void turnOffAllLEDs()
+{
+     digitalWrite(RED_LED_PIN, LOW);
+     digitalWrite(GRN_LED_PIN, LOW);
+     digitalWrite(YLW_LED_PIN, LOW);
+     digitalWrite(BLU_LED_PIN, LOW); 
+}
+
+//Turn on the selected output Y0-Y7. Outoff bounds entry turns all off. 
+void setDecoderLED(int i)
+{
+  if ( i >= 0 && i < 8)
+  {
+    digitalWrite(DECODER_ENABLE, HIGH);
+    digitalWrite(DECODER_LINE0, i & BIT0HI);
+    digitalWrite(DECODER_LINE1, i & BIT1HI);
+    digitalWrite(DECODER_LINE2, i & BIT2HI);
+ 
+  }
+  else
+  {
+    digitalWrite(DECODER_ENABLE, LOW); 
+  }
+  
+}
+
+
+void stepScanningEye()
+{
+ static char eyeIndex = 0;
+ setDecoderLED(eyeIndex++); 
+}
